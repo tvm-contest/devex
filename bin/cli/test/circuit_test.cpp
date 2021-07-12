d817eedd4e4aaa1a959024ce540afc60767cbd29
NoamDev <noam.yem@gmail.com>
2021-07-12 16:46:44 +0300

    Add test

---
+++ bin/cli/test/circuit_test.cpp
@@ -0,0 +1,97 @@
+#define BOOST_TEST_MODULE circuit_test
+
+#include <boost/test/included/unit_test.hpp>
+
+#include <nil/crypto3/algebra/curves/bls12.hpp>
+#include <nil/crypto3/algebra/fields/bls12/base_field.hpp>
+#include <nil/crypto3/algebra/fields/bls12/scalar_field.hpp>
+#include <nil/crypto3/algebra/fields/arithmetic_params/bls12.hpp>
+#include <nil/crypto3/algebra/curves/params/multiexp/bls12.hpp>
+#include <nil/crypto3/algebra/curves/params/wnaf/bls12.hpp>
+
+#include <nil/crypto3/zk/components/blueprint.hpp>
+
+#include "../src/circuit.hpp"
+
+using namespace nil::crypto3;
+using namespace zk::components;
+
+typedef algebra::curves::bls12<381> curve_type;
+typedef typename curve_type::scalar_field_type field_type;
+
+typedef hmac_component<field_type,
+                knapsack_crh_with_bit_out_component<field_type>,
+                knapsack_crh_with_field_out_component<field_type>>
+    Hmac;
+
+BOOST_AUTO_TEST_SUITE(circuit_test_suite)
+
+BOOST_AUTO_TEST_CASE(circuit_test) {
+    std::cout << "Circuit test started" << std::endl;
+
+    std::vector<std::vector<bool>> secrets {
+        {1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1},
+        {1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1},
+        {0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1}
+    };
+
+    std::vector<field_type::value_type> hashes(4);
+
+    for(int i = 0; i < 4; ++i) {
+        hashes[i] = Hmac::get_hmac(secrets[i], std::vector<bool>(circuit::HASH_MSG_LEN, 1)) [0];
+    }
+
+    std::vector<field_type::value_type> hash_list {hashes[0],hashes[1],hashes[2],0,0,0,0,0,0,0};
+
+    std::vector<std::uint32_t> vote_choices {0,1,2,10000000/*,4294967295*/};
+    for(std::uint32_t vote_choice : vote_choices) {
+        for(std::size_t secret_index = 0; secret_index < 4; ++secret_index) {
+            if(vote_choice!=0) {
+                field_type::value_type a = vote_choice;
+                //field_type::value_type::modulus_type b = a.data;
+                std::cout<<nil::crypto3::multiprecision::msb(field_type::value_type::modulus_type(a.data))<<std::endl;
+            }
+            std::vector<bool> vote_choice_bv(circuit::VOTE_MSG_LEN);
+
+            for(std::size_t i = 0, temp = vote_choice; i < circuit::VOTE_MSG_LEN; ++i) {
+                vote_choice_bv[i] = temp&1;
+                temp >>= 1;
+            }
+
+            field_type::value_type vote_choice_hmac =
+                Hmac::get_hmac(secrets[secret_index], vote_choice_bv)[0];
+            field_type::value_type anoymous_id =
+                Hmac::get_hmac(secrets[secret_index], std::vector<bool>(circuit::ANONYMOUS_ID_MSG_LEN, 1))[0];
+
+            for(std::size_t index = 0; index < circuit::MAX_ENTRIES; ++index) {
+                for(int real_vote_hmac = 0; real_vote_hmac < 2; ++real_vote_hmac) {
+                    for(int real_anonymous_id  = 0; real_anonymous_id < 2; ++real_anonymous_id) {
+                        std::cout<< "Testing for secret number:" << index <<
+                            ", vote_choice:" << vote_choice <<
+                            ", real_vote_hmac:" << (real_vote_hmac ? "true": "false") <<
+                            ", real_anonymous_id:" << (real_anonymous_id ? "true": "false") << std::endl;
+                        blueprint<field_type> bp = circuit::generate_circuit_with_witness<field_type>(
+                            hash_list,
+                            secrets[secret_index],
+                            vote_choice,
+                            index,
+                            real_vote_hmac ? vote_choice_hmac : hashes[0],
+                            real_anonymous_id ? anoymous_id : hashes[1]
+                        );
+                        if(secret_index!=3 &&
+                             index == secret_index &&
+                             real_vote_hmac &&
+                             real_anonymous_id) {
+                            BOOST_CHECK(bp.is_satisfied());
+                        } else {
+                            BOOST_CHECK(!bp.is_satisfied());
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
