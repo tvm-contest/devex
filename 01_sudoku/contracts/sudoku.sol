96e13d9964ab7591e9de853c159fa1d06e3dba12
TS <ts@noemailsplease.com>
2021-07-14 23:58:24 +0200

    remove useless getter from Sudoku contract

--- 01_sudoku/contracts/sudoku.sol
+++ 01_sudoku/contracts/sudoku.sol
@@ -76,12 +76,6 @@ function get() public view returns
 	}
     }
 
-    // getter for debugging
-    function get_value(uint i) public view returns (uint8[3] value){
-	return m_instance[i];
-    }
-
-
     // lots of for loops but at least none of this is stored in the
     // contract's memory
     function pi_from_instance(uint8[][3] instance)
