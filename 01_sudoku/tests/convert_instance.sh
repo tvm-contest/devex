b429d7668c88c0164d61a3954f327f8260bc30b3
TS <ts@noemailsplease.com>
2021-07-14 14:45:38 +0200

    finally working matrix input converter

--- 01_sudoku/tests/convert_instance.sh
+++ 01_sudoku/tests/convert_instance.sh
@@ -2,18 +2,20 @@
 
 SUDOKU_SIZE=$(wc -l < "$1")
 IFS='' read -r -d '' string < $1
-#string=tr -d ' ' $string
+string=${string//$'\n'/}
 res=""
 re='^[1-9]'
 for (( i=0; i <$SUDOKU_SIZE; i++ )); do
     for (( j=0; j <$SUDOKU_SIZE; j++ )); do
 	let index=i*$SUDOKU_SIZE+j
-	if [[ '${string:$index:1}' != 0 ]]; then
-	    res="$res, { \"i\" : \"$i\",\"j\" : \"$j\", \"value\" : \"${string:$index:1}\"}"
-	else
-	    echo ${string:$index:1}
+	if [ "${string:$index:1}" != "0" ]; then
+	    if [[ -z "$res" ]]; then
+		sep=""
+	    else
+		sep=","
+	    fi
+	    res="$res$sep { \"i\" : \"$i\",\"j\" : \"$j\", \"value\" : \"${string:$index:1}\"}"
         fi
-
     done
 done
 
