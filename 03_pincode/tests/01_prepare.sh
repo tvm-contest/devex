a6cb28d6ab722069314759cef919bc97cff63dac
Fabrice Le Fessant <fabrice.le_fessant@origin-labs.com>
2021-07-15 15:40:20 +0200

    Add deployment of Pincode

--- 03_pincode/tests/01_prepare.sh
+++ 03_pincode/tests/01_prepare.sh
@@ -2,6 +2,6 @@
 
 . ./env.sh
 
-cmd ../cpp/pincode-client prepare ${PINCODE_PASSPHRASE} || exit 2
+cmd ../cpp/pincode-client prepare "${PINCODE_PASSPHRASE}" || exit 2
 
 zip provkey.bin.zip provkey.bin || exit 2
