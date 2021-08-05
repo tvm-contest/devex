1b5d725231eceba3886ad4057b992b069ab24abf
gofman <eid@freeton-virtualbox.localdomain>
2021-08-05 13:29:16 +0300

    Update

--- contracts/SubscriptionService.sol
+++ contracts/SubscriptionService.sol
@@ -5,20 +5,16 @@ pragma AbiHeader expire;
 
 contract SubscriptionService {
 
-    address static public to;
-    uint128 static public value;
-    uint32 static public period;
+    address static to;
+    uint128 static value;
+    uint32 static  period;
     uint256 public serviceKey;
-    uint256 public tvmPubkey;
-    uint256 public msgPubkey;
 
     constructor(bytes signature) public {
         TvmCell code = tvm.code();
         optional(TvmCell) salt = tvm.codeSalt(code);
         require(salt.hasValue(), 101);
         serviceKey = salt.get().toSlice().decode(uint256);
-        msgPubkey = msg.pubkey();
-        tvmPubkey = tvm.pubkey();
         require(msg.sender != address(0), 101);
         require(tvm.checkSign(tvm.hash(code), signature.toSlice(), tvm.pubkey()), 102);
         require(tvm.checkSign(tvm.hash(code), signature.toSlice(), serviceKey), 103);
