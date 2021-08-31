cb4bd86357b90ce647b1ff00551d07b604ec2f76
Anton Zavodovskiy <azavodovskiy@pixsystem.com>
2021-09-01 00:21:03 +0300

    working tests

--- contracts/tests/run.sh
+++ contracts/tests/run.sh
@@ -3,21 +3,21 @@
 
 echo -e "START testing....\n"
 echo -e "\nDeploying Service.."
-sleep 5
+sleep 10
 expect -f ./TestCreateService.exp
 echo -e "\nGet Info Service.."
-sleep 5
-expect -f ./TestGetInfoService.exp
+sleep 10
+expect -f ./TestGetinfoService.exp
 echo -e "\nDeploy subscription.."
-sleep 5
+sleep 10
 expect -f ./TestDeploySubscription.exp
 echo -e "\nShow subscription.."
-sleep 5
+sleep 10
 expect -f ./TestShowSubscriptions.exp
 echo -e "\nPopup wallet 1 TON"
-sleep 5
+sleep 10
 expect -f ./TestPopupWallet.exp
 echo -e "\nDelete Service.."
-sleep 5
+sleep 10
 expect -f ./TestDeleteService.exp
 echo -e "\nCompleted successfully."
