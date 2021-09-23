68e02f4ffda4a9484cffe522d8202652be3fa1eb
Alexander Salamatov <ch1seL@ya.ru>
2021-09-23 21:47:59 +0300

    init

---
+++ src/Server/Controllers/EndpointController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EndpointController : ControllerBase
+    {
+        private readonly ILogger<EndpointController> _logger;
+
+        public EndpointController(ILogger<EndpointController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public EndpointParameters Receive([FromForm]EndpointParameters endpointParameters)
+        {
+            _logger.LogInformation("Received hash: {Hash} data: {Data}", endpointParameters.Hash, endpointParameters.Data);
+            
+            return endpointParameters;
+        }
+    }
+}
---
+++ src/Server/Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "HTTP Notification Provider API",
+                Version = "v1"
+            }); });
+            services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
+            });
+        }
+    }
+}
