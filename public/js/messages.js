2bcddfa47f5b81ba5cc7e2e6bf058838db59abdb
Nikita Rukavkov <nrukavkov@appulate.com>
2021-09-29 23:27:39 +0300

    changed colors in table

--- public/js/messages.js
+++ public/js/messages.js
@@ -3,6 +3,6 @@ $(document).on('change','#customerId',function(){
 });
 
 function messageStyle(row, index) {
-    if (row.isDelivered) { return { css: { 'background-color': '#bcffa6' } } }
-    else { return { css: { 'background-color': '#fcbdbd' } } }
+    if (row.isDelivered) { return { css: { 'background-color': '#d4edda' } } }
+    else { return { css: { 'background-color': '#f8d7da' } } }
 }
