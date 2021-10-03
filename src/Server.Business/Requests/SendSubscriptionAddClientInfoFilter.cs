cb76986107b32b06b8e5c4279529d39060680cf9
Alexander Salamatov <ch1seL@ya.ru>
2021-10-03 16:05:23 +0300

    feat(Server) skip publishing for message without client info in db

--- src/Server.Business/Requests/SendSubscriptionAddClientInfoFilter.cs
+++ src/Server.Business/Requests/SendSubscriptionAddClientInfoFilter.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using GreenPipes;
 using MassTransit;
+using Microsoft.Extensions.Logging;
 
 namespace Server.Requests
 {
     public class SendSubscriptionAddClientInfoFilter<T> : IFilter<PublishContext<T>> where T : class
     {
+        private readonly ILogger<SendSubscriptionAddClientInfoFilter<T>> _logger;
         private readonly ServerDbContext _serverContext;
 
-        public SendSubscriptionAddClientInfoFilter(ServerDbContext serverContext)
+        public SendSubscriptionAddClientInfoFilter(ServerDbContext serverContext, ILogger<SendSubscriptionAddClientInfoFilter<T>> logger)
         {
             _serverContext = serverContext;
+            _logger = logger;
         }
 
         public async Task Send(PublishContext<T> context, IPipe<PublishContext<T>> next)
@@ -21,11 +23,19 @@ namespace Server.Requests
                 var cancellationToken = context.CancellationToken;
 
                 var clientInfo = await _serverContext.ClientInfos.FindAsync(new object[] { message.Hash }, cancellationToken);
-                if (clientInfo == null) throw new NullReferenceException("Client not found") { Data = { { "ClientHash", message.Hash } } };
-                context.Headers.Set("clientInfo", clientInfo);
-            }
+                if (clientInfo == null)
+                {
+                    _logger.LogWarning("Client {ClientHash} not found in the Database, skip publishing..", message.Hash);
+                    return;
+                }
 
-            await next.Send(context);
+                context.Headers.Set("clientInfo", clientInfo);
+                await next.Send(context);
+            }
+            else
+            {
+                await next.Send(context);
+            }
         }
 
         public void Probe(ProbeContext context)
