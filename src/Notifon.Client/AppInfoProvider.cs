223b17a749c8e2a64c496069fc2cb05a041b75e4
Alexander Salamatov <ch1seL@ya.ru>
2021-10-14 17:10:09 +0300

    fix(Client) save app options to localstorage

--- src/Notifon.Client/AppInfoProvider.cs
+++ src/Notifon.Client/AppInfoProvider.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
 
 namespace Notifon.Client {
     public class AppInfoProvider {
         private readonly Lazy<Task<AppOptions>> _appOptionsLazy;
 
-        public AppInfoProvider(HttpClient httpClient) {
-            var appInfoClient = new ApiAppInfoClient(httpClient);
-            var taskFunc = new Func<Task<AppOptions>>(() => appInfoClient.GetServerStatusAsync());
-            _appOptionsLazy = new Lazy<Task<AppOptions>>(taskFunc);
+        public AppInfoProvider(HttpClient httpClient, ILocalStorageService localStorageService) {
+            _appOptionsLazy = new Lazy<Task<AppOptions>>(async () => {
+                var appInfoClient = new ApiAppInfoClient(httpClient);
+                try {
+                    var options = await appInfoClient.GetServerStatusAsync();
+                    await localStorageService.SetItemAsync("AppOptions", options);
+                    return options;
+                }
+                catch (HttpRequestException) {
+                    var appOptions = await localStorageService.GetItemAsync<AppOptions>("AppOptions");
+                    return appOptions ?? new AppOptions();
+                }
+            });
         }
 
         public async Task<AppOptions> GetAppOptions() {
