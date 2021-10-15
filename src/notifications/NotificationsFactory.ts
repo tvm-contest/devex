35294137057c4fd4d21f6fa1e8f93d6964abcf5a
Anton Solodkov <anton.solodkov@sharekey.com>
2021-10-15 10:54:44 +0300

    add more loggers

--- src/notifications/NotificationsFactory.ts
+++ src/notifications/NotificationsFactory.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
+import Logger from '../lib/console-logger';
 import WebhookConsumer from '../models/WebhookConsumer';
 import Notification from './Notification';
+
+const logger = new Logger();
 
 class NotificationsFactory {
 	private notificationsQueue: Record<string, Array<Notification>> = {};
@@ -10,16 +13,22 @@ class NotificationsFactory {
 		const consumer = await WebhookConsumer.findOne({ hash });
 
 		if (!consumer?.endpoint) {
+			logger.error('Consumer is not defined or has no endpoint');
+
 			return;
 		}
 
 		if (this.notificationsQueue[consumer.endpoint]?.find(notification => notification.key === key)) {
+			logger.error(`Doubled notification with key ${key}`);
+
 			return;
 		}
 
 		const isEndpointValid = consumer.isValidated || (await this.checkEndpointValidation(consumer.endpoint, consumer.uuid));
 
 		if (!isEndpointValid) {
+			logger.error(`Endpoint ${consumer.endpoint} is not validated`);
+
 			return;
 		}
 
@@ -49,20 +58,20 @@ class NotificationsFactory {
 		}
 	}
 
-	private async checkEndpointValidation(endpoint: string, uuid: string) {
+	private checkEndpointValidation = async (endpoint: string, uuid: string) => {
 		const shouldAddSlash = !endpoint.endsWith('/');
 		const urlToCheckValidation = `${endpoint}${shouldAddSlash ? '/' : ''}.well-known/i4ins-validation.txt`;
 
 		try {
 			const response = await axios.get(urlToCheckValidation);
 
-			return response.data === uuid;
+			return response && response.data && response.data === uuid;
 		} catch (e) {
 			return false;
 		}
 	}
 
-	private enqueueNotification(notification: Notification, endpoint: string) {
+	private enqueueNotification = (notification: Notification, endpoint: string) => {
 		if (this.notificationsQueue[endpoint]) {
 			this.notificationsQueue[endpoint].push(notification);
 		} else {
@@ -70,8 +79,10 @@ class NotificationsFactory {
 		}
 	}
 
-	private releaseNotification(endpoint: string): void {
+	private releaseNotification = (endpoint: string) => {
 		this.notificationsQueue[endpoint].shift();
+
+		logger.info('Releasing notification');
 
 		if (this.notificationsQueue[endpoint].length) {
 			const nextNotification = this.notificationsQueue[endpoint][0];
