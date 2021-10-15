fb1a5ded42c177dbfa4d5bce051e5433e5d6ed2c
Alexander Salamatov <ch1seL@ya.ru>
2021-10-15 10:49:35 +0300

    fix(Server) remove static Firebase instance

--- src/Notifon.Server/Program.cs
+++ src/Notifon.Server/Program.cs
@@ -1,8 +1,4 @@
-﻿using System.IO;
-using FirebaseAdmin;
-using FirebaseAdmin.Messaging;
-using Google.Apis.Auth.OAuth2;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
@@ -17,8 +13,6 @@ namespace Notifon.Server {
                 .WriteTo.Console()
                 .CreateBootstrapLogger();
 
-            InitFirebaseApp();
-
             CreateHostBuilder(args)
                 .UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration))
                 .Build()
@@ -31,13 +25,6 @@ namespace Notifon.Server {
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
                     webBuilder.UseSentry();
-                });
-        }
-
-        private static void InitFirebaseApp() {
-            if (FirebaseMessaging.DefaultInstance == null)
-                FirebaseApp.Create(new AppOptions {
-                    Credential = GoogleCredential.FromFile(Path.Combine(Directory.GetCurrentDirectory(), "firebase-key.json"))
                 });
         }
     }
