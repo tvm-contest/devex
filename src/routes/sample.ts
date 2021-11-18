0cb961ed437a7bb74923940c12f117c2d6d7dbfb
Tamara Kobyakova <kobyakovatamarah@gmail.com>
2021-11-18 22:20:09 +0500

    Create

--- src/routes/sample.ts
+++ src/routes/sample.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 const router = express.Router();
+const fs = require('fs');
+import { addFileToIPFS } from '../services/add-ipfs.service';
 
 /* GET sample listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  const file = fs.readFileSync('text.txt', 'utf8');
+  const CID = await addFileToIPFS(file);
+  res.render('my-sample', { title: 'My-sample', CID: CID });
 });
 
 export {router as sampleRouter};
