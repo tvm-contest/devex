f837e786dbfcdaa338772674657ca8d321dede39
Tamara Kobyakova <kobyakovatamarah@gmail.com>
2021-11-19 11:08:56 +0500

    Style changes

--- src/services/add-ipfs.service.ts
+++ src/services/add-ipfs.service.ts
@@ -2,12 +2,12 @@ const IPFS = require('ipfs-core');
 
 class ipfsAddService {
 
-async addFileToIPFS(file) {
-    const ipfs = await IPFS.create();
-    const { cid } = await ipfs.add(file);
-    await ipfs.stop();
-    return cid;
-}
+    async addFileToIPFS(file) {
+        const ipfs = await IPFS.create();
+        const { cid } = await ipfs.add(file);
+        await ipfs.stop();
+        return cid;
+    }
 }
 
 export const { addFileToIPFS } = new ipfsAddService()
