3aca7f73e7d64560d316ab202b22a7265a926a39
Tamara Kobyakova <kobyakovatamarah@gmail.com>
2021-11-19 20:57:26 +0500

    Created folder for sample data, moved sample txt to sample data, changed file path in request

--- src/routes/sample.ts
+++ src/routes/sample.ts
@@ -5,7 +5,7 @@ import { addFileToIPFS } from '../services/add-ipfs.service';
 
 /* GET sample listing. */
 router.get('/', async function(req, res, next) {
-  const file = fs.readFileSync('text.txt', 'utf8');
+  const file = fs.readFileSync('sample-data/textfile-test-ipfs-upload.txt', 'utf8');
   const CID = await addFileToIPFS(file);
   res.render('my-sample', { title: 'My-sample', CID: CID });
 });
