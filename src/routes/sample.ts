17a77efa5ef2f6dcf73010ba8acf5f33777df1aa
AntonGulak <antongulak7@gmail.com>
2021-11-23 23:24:53 +0900

    changes string to acc

--- src/routes/sample.ts
+++ src/routes/sample.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { consoleTerminal } from 'tondev';
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
@@ -14,15 +15,32 @@ router.get('/addFileToIPFS', async function(req, res, next) {
 });
 
 router.get('/deployService', async function(req, res, next) {
-	const solString = path.join(globals.SAMPLE_DATA_PATH, '/stringContract.txt');
-	const d = new DeployFromString(); 
-  const acc = await d.createContractAccount(solString);
-	const deploy = await d.deployMethod(acc);
-  const getTvc = await d.getTvcDecode(acc);
-  console.log(getTvc);
-  const getDabi = await d.getDabi(acc);
-  console.log(getDabi);
-  await d.close();
+	const solString = "pragma ton-solidity >= 0.35.0; pragma AbiHeader expire; contract helloworld {function renderHelloWorld () public pure returns (string) {return 'hello';}}";
+	const d = new DeployFromString();
+  var acc; 
+ 
+  try {
+   acc = await d.createContractAccount(solString);
+  } catch(err) { console.error(err);}
+  finally {
+    //console.log(acc);
+  }
+
+  try {
+    const deploy = await d.deployMethod(acc);
+    const getTvc = await d.getTvcDecode(acc);
+    console.log(getTvc);
+    const getDabi = await d.getDabi(acc);
+    console.log(getDabi);
+
+  } catch(err) {
+    console.error(err);
+
+  } finally {
+    await d.close();
+  }
+
+
 });
 
 
