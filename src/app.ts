d734d81b563b0882025511e06dfe242442fb7bec
Denis Korotkyi <donnikds@gmail.com>
2021-12-01 11:22:29 +0300

    getting all tokens data id by rootNft address

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import {sampleRouter} from './routes/sample';
 import {addParamsRouter} from './routes/add-params-to-nft-root';
 import {rootContractForm} from './routes/root-contract-form';
 import {saveJsonRouter} from './routes/save-json';
+import {tokensDataInfo} from './routes/tokens-data-info';
 
 
 
@@ -34,6 +35,7 @@ app.use('/my-sample', sampleRouter);
 app.use('/add-params', addParamsRouter);
 app.use('/root-contract-form', rootContractForm);
 app.use('/save-json', saveJsonRouter);
+app.use('/tokens-data-info', tokensDataInfo);
 
 app.use((req: Request, res: Response) => {
     res.status(404);
