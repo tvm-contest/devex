48c8fcd8e7f26d8d410ba968c906cdcaed355c17
kolyanchi <81684130+kolyanchi@users.noreply.github.com>
2021-12-08 20:33:07 +0300

    adding tabs

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ import {saveJsonRouter} from './routes/save-json';
 import {tokensDataInfo} from './routes/tokens-data-info';
 import {generateContractRouter} from './routes/generate-contract';
 import { oneTokenInfoRouter } from './routes/one-token-info';
-import {newRootContractForm} from './routes/new-root-contract-form';
+//import {newRootContractForm} from './routes/new-root-contract-form';
 import { parameterForm } from './routes/parameter-form'
 import { collectionListRouter } from './routes/collection-list'
 import { mintRouter } from './routes/mint';
@@ -42,7 +42,7 @@ app.use('/save-json', saveJsonRouter);
 app.use('/one-token-info', oneTokenInfoRouter);
 app.use('/tokens-data-info', tokensDataInfo);
 app.use('/generate-contract', generateContractRouter);
-app.use('/new-root-contract-form', newRootContractForm);
+//app.use('/new-root-contract-form', newRootContractForm);
 app.use('/parameter-form', parameterForm);
 app.use('/collection-list', collectionListRouter);
 app.use('/mint', mintRouter);
