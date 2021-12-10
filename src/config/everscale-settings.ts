b80f77aefd340bcfc6aec68295d1f298885a320b
Denis Korotkyi <donnikds@gmail.com>
2021-12-10 20:48:22 +0300

    settings

--- src/config/everscale-settings.ts
+++ src/config/everscale-settings.ts
@@ -3,11 +3,11 @@ export const everscale_settings = {
         public: "af0115f94c93848ac16afb811e3bb992116b0b85f9a2ac618adae56b5f4e2039",
         secret: "c0152d49dee1e48791f1e4c749abdd1be0026cd2042db21f8ca65d8e905cc87d"  
     },
-    ENDPOINTS: "http://net.ton.dev",
-    SAFE_MULTISIG_ADDRESS: "0:919e875110231ef2e3c60bb2cf48314d150ecf4bfaa46f90a9d22a64c5bd7d92",
+    ENDPOINTS: "http://localhost",
+    SAFE_MULTISIG_ADDRESS: "0:d5f5cfc4b52d2eb1bd9d3a8e51707872c7ce0c174facddd0e06ae5ffd17d2fcd",
     SAFE_MULTISIG_KEYS: {
-        public: "a08cdc5e0b83b083b5b5402b5a2e0aa74d79c0856fa4434ad924d91a04f0a17b",
-        secret: "a84c21012d91a386c3c14759ca11968f06806367c9dadb2fe44723634a147bb0"
+        public: "99c84f920c299b5d80e4fcce2d2054b05466ec9df19532a688c10eb6dd8d6b33",
+        secret: "73b60dc6a5b1d30a56a81ea85e0e453f6957dbfbeefb57325ca9f7be96d3fe1a"
     },
-    AUTHOR_GENERATOR_ADDRESS: "0:919e875110231ef2e3c60bb2cf48314d150ecf4bfaa46f90a9d22a64c5bd7d92"
+    AUTHOR_GENERATOR_ADDRESS: "0:d5f5cfc4b52d2eb1bd9d3a8e51707872c7ce0c174facddd0e06ae5ffd17d2fcd"
 };
