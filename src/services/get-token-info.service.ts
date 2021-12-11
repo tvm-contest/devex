525efa51e3998802aa202db5c7ff6c378a51ee19
GuAndrey <andrejgurov@MacBook-Pro-Andrej.local>
2021-12-11 23:25:26 +0300

    fix tokenInfo

--- src/services/get-token-info.service.ts
+++ src/services/get-token-info.service.ts
@@ -3,18 +3,16 @@ import { signerKeys, TonClient } from "@tonclient/core";
 import { libNode } from "@tonclient/lib-node";
 import path from "path";
 import * as fs from 'fs';
-// const IPFS = require('ipfs-core');
+const IPFS = require('ipfs-core');
 // const getResponse = require('ipfs-http-response');
 import { everscale_settings } from "../config/everscale-settings";
 import { globals } from "../config/globals";
 import { nft_setting } from "../config/standart-nft-setting";
 
 export type TokenInfo = {
-  addrData : string
-  addrRoot : string
-  addrOwner : string
-  addrTrusted : string
-  color : string
+  title: string,
+  value: string,
+  tag: string
 }
 
 export class TokenInfoBuilder {
@@ -35,7 +33,7 @@ export class TokenInfoBuilder {
       this.client.close();
   }
 
-  async getTokenInfo(address: string) : Promise<string[]>{
+  async getTokenInfo(address: string) : Promise<TokenInfo[]>{
 
     let tokenAccount = new Account( { abi: nft_setting.INFO_DATA_ABI } , {
       address: address,
@@ -54,27 +52,53 @@ export class TokenInfoBuilder {
 
     getInfo = await tokenAccount.runLocal("getInfo", {});
 
+    let jsonCollection = await JSON.parse(fs.readFileSync(path.resolve(globals.RESULT_COLLECTION, addrRoot.slice(2), "collectionInfo.json")).toString());
     let output = getInfo.decoded?.output
-    let respons : string[] = []
+    let respons : TokenInfo[] = await this.makeRespons(output, jsonCollection)
 
-    for (let key in output){
-      switch(key){
-        case('addrRoot'): respons.push(`Адрес коллекции: ${output[key]}`) ; break;
-        case('addrOwner'): respons.push(`Адрес владельца: ${output[key]}`); break;
-        case('addrAuthor'): respons.push(`Адрес автора: ${output[key]}`); break;
-        case('addrData'): respons.push(`Адрес токена: ${output[key]}`); break;
-        case('id'): respons.push(`ID: ${output[key]} `); break;
-        case('name'): respons.push(`Название: ${Buffer.from(output[key], 'hex').toString()}`); break;
-        case('url'): respons.push(`URL: ${Buffer.from(output[key], 'hex').toString()}`); break;
-        case('number'): respons.push(`Number: ${output[key]} `); break;
-        case('amount'): respons.push(`Amount:  ${output[key]} `); break;
-        case('nftType'): respons.push(`Редкость: ${Buffer.from(output[key], 'hex').toString()} `); break;
-        default: respons.push(`${key}: ${Buffer.from(output[key], 'hex').toString()} `); break;
-      }
-    }
+    console.log(output)
 
     return respons
 
+  }
+
+  private async makeRespons(output, jsonCollection) : Promise<TokenInfo[]> {
+    let respons : TokenInfo[] = []
+    respons.push({title: "Адрес коллекции", value: output['addrRoot'], tag: 'p'})
+    respons.push({title: "Адрес владельца", value: output['addrOwner'], tag: 'p'})
+    respons.push({title: "Адрес автора", value: output['addrAuthor'], tag: 'p'})
+    respons.push({title: "Адрес токена", value: output['addrData'], tag: 'p'})
+    
+    if (jsonCollection.collection.rarities != []){
+      let value = Buffer.from(output['nftType'], 'hex').toString()
+      respons.push({title: "Тип токена", value: value, tag: 'p'})
+    }
+
+    for (let parametr of jsonCollection.collection.parameters) {
+      if (parametr.type == 'uint') {
+
+        let value = Number(output['_' + parametr.name]).toString()
+        respons.push({title: parametr.name, value: value, tag: 'p'})
+
+      } else if (parametr.type == 'string') {
+
+        let value = Buffer.from(output['_' + parametr.name], 'hex').toString()
+        let tag = 'p'
+
+        if (value.match(/ipfs.io\/ipfs/g)) {
+          tag = 'iframe'
+        }
+
+        respons.push({title: parametr.name, value: value, tag: tag})
+      } 
+    }
+    
+    for (let _enum of jsonCollection.enums) {
+      let value = _enum.enumVariants[Number(output['_enum' + _enum.name])]
+      respons.push({title: _enum.name, value: value, tag: 'p'})
+    }
+    
+    return respons
   }
 
   
