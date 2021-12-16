228ff586266a4797d7fbe9a442b2f892a45c2717
Denis Korotkyi <donnikds@gmail.com>
2021-12-16 16:09:33 +0300

    directSale

--- src/routes/sample.ts
+++ src/routes/sample.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
-import { globals } from '../config/globals'
+import { globals } from '../config/globals';
+import { everscale_settings } from '../config/everscale-settings';
+import { addFileToIPFS } from '../services/add-ipfs.service';
 import { DeployService } from '../services/deploy.service';
 import { DeployTrueNftService } from '../services/deployTrueNft.service';
+import { DirectSaleService } from '../services/directSale.service';
 const router = express.Router();
 
 router.get('/deployService', async function (req, res, next) {
@@ -40,6 +43,17 @@ router.get('/deployTrueNftService', async function (req, res, next) {
   const deployTrueNftService = new DeployTrueNftService();
   const testPath = path.resolve(globals.BASE_PATH, "src" ,"sample-data", "trueNftSample");
   // deployTrueNftService.deployTrueNft(testPath);
+});
+
+router.get('/directSale', async function (req, res, next) {
+  const directSaleService = new DirectSaleService();
+  let addrRoyaltyAgent = everscale_settings.SAFE_MULTISIG_ADDRESS;
+  let addressRootNft = "4db0ff55de68ef386cebecc285532dab1e497e1bfaae77a4b5b40049151745b1";
+  //let rootAddr = await directSaleService.deployDirectSaleRoot(addressRootNft, addrRoyaltyAgent, 5);
+  //console.log(rootAddr);        // 0:eb1e65768afad81c7487e15b1bd778b0e9df6fdf9e2e9a0a6c796328f88e87ac
+  let addressNft = "0:b8d68a19e04b589e189b7a39fe8ed28779c00381f909c605b06928bea59bd5a4";
+  
+  await directSaleService.deployDirectSale(addressRootNft, addressNft);
 });
 
 export { router as sampleRouter };
---
+++ src/services/directSale.service.ts
@@ -0,0 +1,182 @@
+import * as fs from 'fs';
+import path from 'path';
+import { Account } from '@tonclient/appkit';
+import { libNode } from '@tonclient/lib-node';
+import { TonClient } from '@tonclient/core';
+import { globals } from '../config/globals';
+import { everscale_settings } from '../config/everscale-settings';
+import { DeployService } from './deploy.service';
+
+export class DirectSaleService {
+    private deployService: DeployService;
+    private client: TonClient;
+
+    constructor() {
+        TonClient.useBinaryLibrary(libNode);
+
+        this.client = new TonClient({
+            network: {
+                endpoints: [everscale_settings.ENDPOINTS]
+            }
+        });
+        this.deployService = new DeployService();
+    }
+
+    async deployDirectSaleRoot(collectionPath: string, addrRoyaltyAgent: string, royaltyPercent: number) : Promise<string> {
+        let directSaleRootAcc = await this.createDirectSaleRootAccount(collectionPath);
+        let directSaleAcc = await this.createDirectSaleAccount(collectionPath);
+        let initInput = await this.buildInitInputForDeployDirectSaleRoot(directSaleAcc, addrRoyaltyAgent, royaltyPercent);
+        await this.deployService.deploy(directSaleRootAcc, initInput);
+         
+        return await directSaleRootAcc.getAddress();
+    }
+    
+    async deployDirectSale(collectionPath: string, nftAddr: string) : Promise<void> {
+        let nftAbi = await JSON.parse(fs.readFileSync(path.join(globals.RESULT_COLLECTION, collectionPath, "Data.abi.json")).toString());
+        let nftTvc = fs.readFileSync(path.resolve(path.join(globals.RESULT_COLLECTION, collectionPath, "Data.tvc")), {encoding: 'base64'});
+        let nftAcc = new Account({
+            abi: nftAbi,
+            tvc: nftTvc
+        }, {
+            address: nftAddr,
+            client: this.client,
+            signer: {
+                type: "Keys",
+                keys: everscale_settings.SAFE_MULTISIG_KEYS
+            }
+        });
+        let directSaleRootAcc = await this.createDirectSaleRootAccount(collectionPath);
+        let directSaleRootAddr = await directSaleRootAcc.getAddress();
+        let walletAcc = await this.getWalletAcc();
+        
+        await this.landOwnership(nftAcc, walletAcc, directSaleRootAddr, nftAddr);
+        console.log("landOwnership");
+        await this.createSale(directSaleRootAcc, walletAcc, directSaleRootAddr, nftAddr);
+        console.log("createSale");
+        await(20000)
+        let saleAddr = await this.getSaleAddr(directSaleRootAcc, nftAddr);
+        console.log(saleAddr);
+    }
+    
+    async getSaleAddr(directSaleRootAcc: Account, nftAddr) : Promise<any> {
+        let saleAddr = await directSaleRootAcc.runLocal('getSaleAddress',
+        {
+            'addrOwner': everscale_settings.SAFE_MULTISIG_ADDRESS,
+            'addrNft': nftAddr
+        });
+        if (saleAddr.decoded !== undefined) {
+            if (saleAddr.decoded.out_messages) {
+                return saleAddr.decoded.output.addrSale;
+            }
+        }
+        else return ""
+    }
+    
+    private async createDirectSaleRootAccount(collectionPath: string) : Promise<Account> {
+        let pathDirectSale = path.join(globals.RESULT_COLLECTION, collectionPath, "DirectSaleRoot.sol");
+        let directSaleRootCode = fs.readFileSync(pathDirectSale, 'utf8');
+        let directSaleRootAcc= await this.deployService.createContractAccount(directSaleRootCode, path.join(globals.RESULT_COLLECTION, collectionPath), "DirectSaleRoot");
+        return directSaleRootAcc;
+    }
+
+    private async createDirectSaleAccount(collectionPath: string) : Promise<Account> {
+        let pathDirectSale = path.join(globals.RESULT_COLLECTION, collectionPath, "DirectSale.sol");
+        let directSaleCode = fs.readFileSync(pathDirectSale, 'utf8');
+        let directSaleAcc= await this.deployService.createContractAccount(directSaleCode,  path.join(globals.RESULT_COLLECTION, collectionPath), "DirectSale");
+        return directSaleAcc;
+    }
+    
+    private async buildInitInputForDeployDirectSaleRoot(directSaleAcc: Account, addrRoyaltyAgent: string, royaltyPercent: number) : Promise<object> {
+        console.log((await this.deployService.getDecodeTVC(directSaleAcc)).code);
+        let initInput = {
+            codeSale: (await this.deployService.getDecodeTVC(directSaleAcc)).code,
+            addrRoyaltyAgent: addrRoyaltyAgent,
+            royaltyPercent: royaltyPercent
+        };
+        return initInput;
+    }
+
+    private async landOwnership(nftAcc: Account, walletAcc: Account, directSaleRootAddr: string, addrNft: string) {
+        let { body } = await this.client.abi.encode_message_body({
+            abi: nftAcc.abi,
+            signer: {
+                type: "Keys",
+                keys: everscale_settings.SAFE_MULTISIG_KEYS
+            },
+            is_internal: true,
+            call_set: {
+                function_name: "lendOwnership",
+                input: {
+                    _addr: directSaleRootAddr
+                }
+            },
+        });
+        try {
+            await walletAcc.run(
+                "sendTransaction",
+                {
+                    dest: addrNft,
+                    value: 100_000_000,
+                    flags: 3,
+                    bounce: true,
+                    payload: body,
+                }
+            );
+        } catch(err) {
+            console.log("landOwnership error");
+        }
+    }
+
+    private async createSale(directSaleRootAcc: Account, walletAcc: Account, directSaleRootAddr: string, addrNft: string) {
+        let { body } = await this.client.abi.encode_message_body({
+            abi: directSaleRootAcc.abi,
+            signer: {
+                type: "Keys",
+                keys: everscale_settings.SAFE_MULTISIG_KEYS
+            },
+            is_internal: true,
+            call_set: {
+                function_name: "createSale",
+                input: {
+                    addrNft: addrNft
+                }
+            },
+            processing_try_index: 3
+        });
+        try {
+            await walletAcc.run(
+                "sendTransaction",
+                {
+                    dest: directSaleRootAddr,
+                    value: 2_000_000_000,
+                    flags: 3,
+                    bounce: true,
+                    payload: body,
+                }
+            );
+        } catch(err) {
+            console.log("CreateSale error")
+        }
+        
+    }
+
+    private async getWalletAcc() : Promise<Account> {
+        let walletAbi = await JSON.parse(fs.readFileSync(path.resolve(globals.SAMPLE_DATA_PATH, "safeMultisigWallet", "SafeMultisigWallet.abi.json")).toString());
+        let walletTvc = fs.readFileSync(path.resolve(globals.SAMPLE_DATA_PATH, "safeMultisigWallet", "SafeMultisigWallet.tvc"), {encoding: 'base64'});
+        const walletAcc = new Account(
+            {
+                abi: walletAbi, 
+                tvc: walletTvc
+            },
+            {
+                client: this.client,
+                address: everscale_settings.SAFE_MULTISIG_ADDRESS,
+                signer: {
+                    type: "Keys",
+                    keys: everscale_settings.SAFE_MULTISIG_KEYS
+                }
+            }
+        );
+        return walletAcc;
+    }
+}
