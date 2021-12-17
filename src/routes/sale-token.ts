e5add1c918b93bf22c832af01e01e7a12c2f728c
Denis Korotkyi <donnikds@gmail.com>
2021-12-17 12:34:56 +0300

    delete log

--- src/routes/sale-token.ts
+++ src/routes/sale-token.ts
@@ -7,10 +7,6 @@ router.post('/', async function(req, res, next) {
     res.render('token-sale',{rootAddress :req.body.rootAddress,tokenAddress:req.body.tokenAddress});
   });
 router.post('/sale-token-form', async function(req, res, next) {
-    //console.log(req.body.rootAddress)
-    //console.log(req.body.tokenAddress)
-    //console.log(req.body.tokenPrice)
-
     const directSaleService = new DirectSaleService();
     let RootNftAddr = new String(req.body.rootAddress).slice(2);
     console.log("path collection" + RootNftAddr)
